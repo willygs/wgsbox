8018e74b3ba53609310cebe7c54727a683eca50e
willy <willyganjarsaputra@gmail.com>
2015-07-28 08:28:13 +0700

    index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>WGSBox - A Jquery Lightbox</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css" media="all" />
+	<link rel="stylesheet" type="text/css" href="css/wgsbox.css" media="all" />
+</head>
+<body>
+<div id="wraper">
+	<div id="lightbox">
+		<h1>WGSBox - A Jquery Lightbox</h1>
+		<ul>
+			<li><img src="img/thumb_1.jpg" data-large="img/large_1.jpg" alt="img" /></li>
+			<li><img src="img/thumb_2.jpg" data-large="img/large_2.jpg" alt="img" /></li>
+			<li><img src="img/thumb_3.jpg" data-large="img/large_3.jpg" alt="img" /></li>
+			<li><img src="img/thumb_4.png" data-large="img/large_4.png" alt="img" /></li>
+			<li><img src="img/thumb_5.jpg" data-large="img/large_5.jpg" alt="img" /></li>
+		</ul>
+	</div>
+</div>
+<script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
+<script type="text/javascript" src="js/wgsbox.js"></script>
+</body>
+</html>
