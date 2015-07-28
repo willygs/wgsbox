95c8b8fd55eae6b2ec13889b4e595090c9347048
willy <willyganjarsaputra@gmail.com>
2015-07-28 14:22:51 +0700

    index.php

--- index.php
+++ index.php
@@ -7,15 +7,16 @@
 	<link rel="stylesheet" type="text/css" href="css/wgsbox.css" media="all" />
 </head>
 <body>
+
 <div id="wraper">
 	<div id="lightbox">
 		<h1>WGSBox - A Jquery Lightbox</h1>
 		<ul>
-			<li><img src="img/thumb_1.jpg" data-large="img/large_1.jpg" alt="img" /></li>
-			<li><img src="img/thumb_2.jpg" data-large="img/large_2.jpg" alt="img" /></li>
-			<li><img src="img/thumb_3.jpg" data-large="img/large_3.jpg" alt="img" /></li>
-			<li><img src="img/thumb_4.png" data-large="img/large_4.png" alt="img" /></li>
-			<li><img src="img/thumb_5.jpg" data-large="img/large_5.jpg" alt="img" /></li>
+			<li><img src="img/thumb_1.jpg" data-large="img/large_1.jpg" alt="img" data-title="Dont Worry Be Happy" /></li>
+			<li><img src="img/thumb_2.jpg" data-large="img/large_2.jpg" alt="img" data-title="Pemandangan"/></li>
+			<li><img src="img/thumb_3.jpg" data-large="img/large_3.jpg" alt="img" data-title="Abstrak"/></li>
+			<li><img src="img/thumb_4.png" data-large="img/large_4.png" alt="img" data-title="Jalanan"/></li>
+			<li><img src="img/thumb_5.jpg" data-large="img/large_5.jpg" alt="img" data-title="I Will Survive"/></li>
 		</ul>
 	</div>
 </div>
